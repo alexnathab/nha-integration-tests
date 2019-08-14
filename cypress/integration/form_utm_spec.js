bca9b5967a0efe4606a2e38dbd1dc4b0907cc72b
Alex Thornton <alext@nathab.com>
2019-08-14 14:16:06 -0600

    test passes

--- cypress/integration/form_utm_spec.js
+++ cypress/integration/form_utm_spec.js
@@ -31,32 +31,23 @@ var dev_sites = [{
 describe(`Secret marketo form UTM inputs get filled out based on queries`, function () {
     it(`Must fill out secret marketo form or it will fail`, function () {
         //the following string is full of queries and thus is being used for testing
-        cy.visit(`alex.nathab.com/africa/the-great-namibia-wildlife-safari/?banner&utmsource=TEST&utmmedium=TEST&utmcontent=TEST&utmcampaign=TEST&mkt_tok=eyJpIjoiTUdRelpEQTFZelpoTVRSbSIsInQiOiJiZFJtVnRJb1BRY1dmc0luTUltSnVsOE9qODlmR3l6dzJObUxoeFM4aUt1U2FrUmx4bEtuRGcydzl5Tlc5VHNncHRkajd5dGxITkFSUDhhOXV5S1FhQysybnJTWlpkRWtueERsSW1reTlkeDAyRGZ4elhzdTJYTmZYUnlja3ZVZyJ9`);
+        cy.visit(`alex.nathab.com/africa/the-great-namibia-wildlife-safari/?banner&utm_source=TEST&utm_medium=TEST&utm_content=TEST&utm_campaign=TEST`);
         cy.get(`${cta_buttons[0].id}`).click();
         cy.contains(`${cta_buttons[0].contains}`);
-        cy.get('[test="pdf_lightbox"]').find('#form-pdf').should('be.visible');
+        cy.get('[test=pdf_lightbox]').find('#form-pdf').should('be.visible');
         cy.get('#form-pdf').find('#first_name').type('TEST');
         cy.get('#form-pdf').find('#last_name').type('TEST');
-        cy.get('#form-pdf').find('#email').type('test@solocreativeco.com');
+        cy.get('#form-pdf').find('#email').type('test@solocreative.co');
         cy.get('#form-pdf').find('#requestCatalog').check();
         cy.get('#form-pdf').find('#pdfRequestHomeStreet').type('TEST');
         cy.get('#form-pdf').find('#pdfRequestHomeCity').type('TEST');
         cy.get('#form-pdf').find('#pdfRequestHomeState').select('Colorado');
         cy.get('#form-pdf').find('#pdfRequestHomeZIP').type('TEST');
         cy.get('#form-pdf-submit').click();
-        cy.get('[test="utm_form"]').find('#utmcampaign').type('TEST');
-        if (cy.url().should('include', 'utmcampaign')) {
-            cy.get('[test="utm_form"]').find('#utmcampaign').should('have.value', 'TEST');
-        } else if (cy.url().should('include', 'utmcontent')) {
-            cy.get('[test="utm_form"]').find('#utmcontent').should('have.value', 'TEST');
-        } else if (cy.url().should('include', 'utmmedium')) {
-            cy.get('[test="utm_form"]').find('#utmmedium').should('have.value', 'TEST');
-        } else if (cy.url().should('include', 'utmsource')) {
-            cy.get('[test="utm_form"]').find('#utmsource').should('have.value', 'TEST');
-        } else if (cy.url().should('include', 'utmterm')) {
-            cy.get('[test="utm_form"]').find('#utmterm').should('have.value', 'TEST');
-        }
-        // } else if (cy.url().contains('utm_source'))
-        // cy.get('#cypress_utm_testing').find('#recentConversionAction').should('have.value', 'TEST');
+        cy.wait(5000)
+        cy.get('[test=utm_form]').find('#utmcampaign').should('not.have.value', '');
+        cy.get('[test=utm_form]').find('#utmcontent').should('not.have.value', '');
+        cy.get('[test=utm_form]').find('#utmmedium').should('not.have.value', '');
+        cy.get('[test=utm_form]').find('#utmsource').should('not.have.value', '');
     })
 });
