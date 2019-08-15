5d185cb08af92019bef3e3de1f4388ca52c23312
Alex Thornton <alext@nathab.com>
2019-08-15 16:51:03 -0600

    added empty field checks
    to forms that shouldn't populate any fields

--- cypress/integration/ask_cta_spec.js
+++ cypress/integration/ask_cta_spec.js
@@ -27,6 +27,41 @@ var cta_buttons = {
 
 // TEST 1 eNewsletter CHECKED
 
+// describe(`Trip Internal Link CTA Test from overview page / detail button`, function () {
+//     it(`Clicks details CTA, fills out the form, and submits it`, function () {
+//         cy.visit(`${dev_sites[0].prefix}nathab.com/africa/the-great-namibia-wildlife-safari/?banner&utm_source=testutmsrc&utm_medium=testurmmed&utm_content=testutmcont&utm_campaign=testutmcamp&utm_term=TEST`);
+//         cy.get(`${cta_buttons.id}`).click();
+//         cy.contains(`${cta_buttons.contains}`);
+//         cy.get(`[ctest=ask_cta_lightbox]`).find('#form-ask').should('be.visible');
+//         cy.get('#form-ask').find('#first_name').type('Alex-Test');
+//         cy.get('#form-ask').find('#last_name').type('Test');
+//         cy.get('#form-ask').find('#informationRequestPhone').type('Test');
+//         cy.get('#form-ask').find('#email').type('test@solocreative.co');
+//         // cy.get('#form-ask').find('#requestNewsletter').uncheck();
+//         cy.get('#form-ask').find('#informationRequestDestinationsTA').clear()
+//         cy.get('#form-ask').find('#informationRequestDestinationsTA').type('THIS IS A TEST');
+//         cy.get('#form-ask-submit').click()
+//         //pulled from ASK form
+//         cy.get('[ctest=utm_form]').find('#FirstName').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#LastName').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#Email').should('not.have.value', '');
+//         //OPT IN checkboxes
+//         cy.get('[ctest=utm_form]').find('#optInNHA').should('be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInTripDrips').should('be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInStories').should('be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInWebinar').should('be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInSurveys').should('be.checked');
+//         //pulled from UTM codes in URL for google analytics 
+//         cy.get('[ctest=utm_form]').find('#utmcampaign').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmcontent').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmmedium').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmsource').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmterm').should('not.have.value', '');
+//     })
+// })
+
+// TEST 2 eNewsletter NOT CHECKED
+
 describe(`Trip Internal Link CTA Test from overview page / detail button`, function () {
     it(`Clicks details CTA, fills out the form, and submits it`, function () {
         cy.visit(`${dev_sites[0].prefix}nathab.com/africa/the-great-namibia-wildlife-safari/?banner&utm_source=testutmsrc&utm_medium=testurmmed&utm_content=testutmcont&utm_campaign=testutmcamp&utm_term=TEST`);
@@ -37,25 +72,25 @@ describe(`Trip Internal Link CTA Test from overview page / detail button`, funct
         cy.get('#form-ask').find('#last_name').type('Test');
         cy.get('#form-ask').find('#informationRequestPhone').type('Test');
         cy.get('#form-ask').find('#email').type('test@solocreative.co');
-        // cy.get('#form-ask').find('#requestNewsletter').uncheck();
+        cy.get('#form-ask').find('#requestNewsletter').uncheck();
         cy.get('#form-ask').find('#informationRequestDestinationsTA').clear()
         cy.get('#form-ask').find('#informationRequestDestinationsTA').type('THIS IS A TEST');
         cy.get('#form-ask-submit').click()
         //pulled from ASK form
-        cy.get('[ctest=utm_form]').find('#FirstName').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#LastName').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#Email').should('not.have.value', '');
+        cy.get('[ctest=utm_form]').find('#FirstName').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#LastName').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#Email').should('have.value', '');
         //OPT IN checkboxes
-        cy.get('[ctest=utm_form]').find('#optInNHA').should('be.checked');
-        cy.get('[ctest=utm_form]').find('#optInTripDrips').should('be.checked');
-        cy.get('[ctest=utm_form]').find('#optInStories').should('be.checked');
-        cy.get('[ctest=utm_form]').find('#optInWebinar').should('be.checked');
-        cy.get('[ctest=utm_form]').find('#optInSurveys').should('be.checked');
+        cy.get('[ctest=utm_form]').find('#optInNHA').should('not.be.checked');
+        cy.get('[ctest=utm_form]').find('#optInTripDrips').should('not.be.checked');
+        cy.get('[ctest=utm_form]').find('#optInStories').should('not.be.checked');
+        cy.get('[ctest=utm_form]').find('#optInWebinar').should('not.be.checked');
+        cy.get('[ctest=utm_form]').find('#optInSurveys').should('not.be.checked');
         //pulled from UTM codes in URL for google analytics 
-        cy.get('[ctest=utm_form]').find('#utmcampaign').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#utmcontent').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#utmmedium').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#utmsource').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#utmterm').should('not.have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmcampaign').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmcontent').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmmedium').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmsource').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmterm').should('have.value', '');
     })
 })
--- cypress/integration/pdf_cta_spec.js
+++ cypress/integration/pdf_cta_spec.js
@@ -37,6 +37,26 @@ var dev_sites = [{
 //         cy.get('#form-pdf').find('#email').type('test@solocreative.co');
 //         cy.get('#form-pdf').find('#requestNewsletter').uncheck();
 //         cy.get('#form-pdf-submit').click();
+
+//         cy.get('[ctest=utm_form]').find('#FirstName').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#LastName').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#Email').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#LeadSource').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#sourceDetail').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#recentTripPDFURL').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#temp').should('have.value', '');
+
+//         cy.get('[ctest=utm_form]').find('#optInNHA').should('not.be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInTripDrips').should('not.be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInStories').should('not.be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInWebinar').should('not.be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInSurveys').should('not.be.checked');
+
+//         cy.get('[ctest=utm_form]').find('#utmcampaign').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmcontent').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmmedium').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmsource').should('have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmterm').should('have.value', '');
 //     });
 // });
 
@@ -75,28 +95,6 @@ var dev_sites = [{
 // });
 
 // TEST 3 -- cat checked only 
-// describe(`Secret marketo form UTM inputs get filled out based on queries`, function () {
-//     it(`Must fill out secret marketo form or it will fail`, function () {
-//         //the following string is full of queries and thus is being used for testing
-//         cy.visit(`alex.nathab.com/africa/the-great-namibia-wildlife-safari/?banner&utm_source=TEST&utm_medium=TEST&utm_content=TEST&utm_campaign=TEST&utm_term=TEST`);
-//         cy.get(`${cta_buttons.id}`).click();
-//         cy.contains(`${cta_buttons.contains}`);
-//         cy.get('[test=pdf_lightbox]').find('#form-pdf').should('be.visible');
-//         cy.get('#form-pdf').find('#first_name').type('TEST');
-//         cy.get('#form-pdf').find('#last_name').type('TEST');
-//         cy.get('#form-pdf').find('#email').type('test@solocreative.co');
-//         cy.get('#form-pdf').find('#requestNewsletter').uncheck();
-//         cy.get('#form-pdf').find('#requestCatalog').check();
-//         cy.get('#form-pdf').find('#pdfRequestHomeStreet').type('TEST');
-//         cy.get('#form-pdf').find('#pdfRequestHomeCity').type('TEST');
-//         cy.get('#form-pdf').find('#pdfRequestHomeState').select('Colorado');
-//         cy.get('#form-pdf').find('#pdfRequestHomeZIP').type('TEST');
-//         cy.get('#form-pdf').find('#pdfRequestHomeCountry').select('Albania');
-//         cy.get('#form-pdf-submit').click();
-//     });
-// });
-
-//TEST 4 -- both enews and catalog checked
 describe(`Secret marketo form UTM inputs get filled out based on queries`, function () {
     it(`Must fill out secret marketo form or it will fail`, function () {
         //the following string is full of queries and thus is being used for testing
@@ -107,6 +105,7 @@ describe(`Secret marketo form UTM inputs get filled out based on queries`, funct
         cy.get('#form-pdf').find('#first_name').type('TEST');
         cy.get('#form-pdf').find('#last_name').type('TEST');
         cy.get('#form-pdf').find('#email').type('test@solocreative.co');
+        cy.get('#form-pdf').find('#requestNewsletter').uncheck();
         cy.get('#form-pdf').find('#requestCatalog').check();
         cy.get('#form-pdf').find('#pdfRequestHomeStreet').type('TEST');
         cy.get('#form-pdf').find('#pdfRequestHomeCity').type('TEST');
@@ -115,24 +114,65 @@ describe(`Secret marketo form UTM inputs get filled out based on queries`, funct
         cy.get('#form-pdf').find('#pdfRequestHomeCountry').select('Albania');
         cy.get('#form-pdf-submit').click();
 
-        cy.get('[ctest=utm_form]').find('#FirstName').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#LastName').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#Email').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#LeadSource').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#sourceDetail').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#recentTripPDFURL').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#temp').should('not.have.value', '');
+        cy.get('[ctest=utm_form]').find('#FirstName').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#LastName').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#Email').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#LeadSource').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#sourceDetail').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#recentTripPDFURL').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#temp').should('have.value', '');
 
-        cy.get('[ctest=utm_form]').find('#optInNHA').should('be.checked');
-        cy.get('[ctest=utm_form]').find('#optInTripDrips').should('be.checked');
-        cy.get('[ctest=utm_form]').find('#optInStories').should('be.checked');
-        cy.get('[ctest=utm_form]').find('#optInWebinar').should('be.checked');
-        cy.get('[ctest=utm_form]').find('#optInSurveys').should('be.checked');
+        cy.get('[ctest=utm_form]').find('#optInNHA').should('not.be.checked');
+        cy.get('[ctest=utm_form]').find('#optInTripDrips').should('not.be.checked');
+        cy.get('[ctest=utm_form]').find('#optInStories').should('not.be.checked');
+        cy.get('[ctest=utm_form]').find('#optInWebinar').should('not.be.checked');
+        cy.get('[ctest=utm_form]').find('#optInSurveys').should('not.be.checked');
 
-        cy.get('[ctest=utm_form]').find('#utmcampaign').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#utmcontent').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#utmmedium').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#utmsource').should('not.have.value', '');
-        cy.get('[ctest=utm_form]').find('#utmterm').should('not.have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmcampaign').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmcontent').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmmedium').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmsource').should('have.value', '');
+        cy.get('[ctest=utm_form]').find('#utmterm').should('have.value', '');
     });
 });
+
+//TEST 4 -- both enews and catalog checked
+// describe(`Secret marketo form UTM inputs get filled out based on queries`, function () {
+//     it(`Must fill out secret marketo form or it will fail`, function () {
+//         //the following string is full of queries and thus is being used for testing
+//         cy.visit(`alex.nathab.com/africa/the-great-namibia-wildlife-safari/?banner&utm_source=TEST&utm_medium=TEST&utm_content=TEST&utm_campaign=TEST&utm_term=TEST`);
+//         cy.get(`${cta_buttons.id}`).click();
+//         cy.contains(`${cta_buttons.contains}`);
+//         cy.get('[test=pdf_lightbox]').find('#form-pdf').should('be.visible');
+//         cy.get('#form-pdf').find('#first_name').type('TEST');
+//         cy.get('#form-pdf').find('#last_name').type('TEST');
+//         cy.get('#form-pdf').find('#email').type('test@solocreative.co');
+//         cy.get('#form-pdf').find('#requestCatalog').check();
+//         cy.get('#form-pdf').find('#pdfRequestHomeStreet').type('TEST');
+//         cy.get('#form-pdf').find('#pdfRequestHomeCity').type('TEST');
+//         cy.get('#form-pdf').find('#pdfRequestHomeState').select('Colorado');
+//         cy.get('#form-pdf').find('#pdfRequestHomeZIP').type('TEST');
+//         cy.get('#form-pdf').find('#pdfRequestHomeCountry').select('Albania');
+//         cy.get('#form-pdf-submit').click();
+
+//         cy.get('[ctest=utm_form]').find('#FirstName').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#LastName').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#Email').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#LeadSource').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#sourceDetail').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#recentTripPDFURL').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#temp').should('not.have.value', '');
+
+//         cy.get('[ctest=utm_form]').find('#optInNHA').should('be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInTripDrips').should('be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInStories').should('be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInWebinar').should('be.checked');
+//         cy.get('[ctest=utm_form]').find('#optInSurveys').should('be.checked');
+
+//         cy.get('[ctest=utm_form]').find('#utmcampaign').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmcontent').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmmedium').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmsource').should('not.have.value', '');
+//         cy.get('[ctest=utm_form]').find('#utmterm').should('not.have.value', '');
+//     });
+// });
