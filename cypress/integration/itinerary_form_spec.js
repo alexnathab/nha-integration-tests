994e90cb9de64c1f8d4ecaa7d2b6d607ecd764e7
Alex Thornton <alext@nathab.com>
2019-10-14 10:10:18 -0600

    fixed itinerary test

--- cypress/integration/itinerary_form_spec.js
+++ cypress/integration/itinerary_form_spec.js
@@ -41,26 +41,13 @@ var dev_sites = [{
 
 describe(`Itinerary Pop Up Form Test - nothing checked`, function () {
     console.log('this is changing');
-    // it(`changes viewport height and width`,
-    //     function () {
-    //         beforeEach(function () {
-    //             cy.viewport(1440, 980);
-    //         });
-    //     });
     beforeEach(function () {
         Cypress.Cookies.preserveOnce('__utma', '__utmb', '__utmc', '__utmt', '__utmz');
     })
     getURL(`${dev_sites[2].prefix}`, '');
-    // clickCTA(`[tripnav-section=text]`);
-    // // it('Makes sure that the item being clicked is the right one', function () {
-    // //     cy.contains(`Trip Details, Dates & Pricing`);
-    // // })
-    checkVisibility('.trip-nav-ul', '#trip-nav-ul-li-2');
-    // it(`Clicks the appropriate button`, function () {
-    //     cy.get('.trip-nav-ul').find('#trip-nav-ul-li-2').get('#trip-nav-ul-li-2.a').click();
-    // });
-    it(`Makes sure the form is visible`, function () {
-        cy.get('.trip-nav-ul').find('#trip-nav-ul-li-2').click();
+    checkVisibility('[tripnav~="wrapper"]', '#trip-nav-ul-li-2');
+    it(`Clicks the appropriate button`, function () {
+        cy.get('[tripnav~="wrapper"]').find('#trip-nav-ul-li-2').click();
     });
     checkVisibility('[ctest=utm_itinerary]', '#form-itinerary');
     submitForm('#form-itinerary-submit');
@@ -74,27 +61,26 @@ describe(`Itinerary Pop Up Form Test - nothing checked`, function () {
 });
 
 // //TEST 2 -- catalog box CHECKED
-// describe(`Itinerary Pop Up Form Test - Catalog checked`, function () {
-//     beforeEach(function () {
-//         Cypress.Cookies.preserveOnce('__utma', '__utmb', '__utmc', '__utmt', '__utmz');
-//     })
-//     getURL(`${dev_sites[2].prefix}`, '');
-//     clickCTA(`[tripnav-section=text]`);
-//     it('Makes sure that the item being clicked is the right one', function () {
-//         cy.contains(`Trip Details, Dates & Pricing`);
-//     })
-//     clickCTA(`${cta_buttons.id}`);
-//     checkVisibility('[ctest=utm_itinerary]', '#form-itinerary');
-//     submitForm('#form-itinerary-submit');
-//     checkValidity('#form-itinerary');
-//     requiredInfo('#form-itinerary');
-//     checkbox('#form-itinerary', '#requestCatalog');
-//     shipInfo('#form-itinerary');
-//     submitForm('#form-itinerary-submit');
-//     utmPersonalInfoTrue();
-//     utmSecondaryTrue();
-//     catTemp();
-//     utmOptInTrue();
-//     utmValueTrue();
+describe(`Itinerary Pop Up Form Test - Catalog checked`, function () {
+    beforeEach(function () {
+        Cypress.Cookies.preserveOnce('__utma', '__utmb', '__utmc', '__utmt', '__utmz');
+    })
+    getURL(`${dev_sites[2].prefix}`, '');
+    checkVisibility('[tripnav~="wrapper"]', '#trip-nav-ul-li-2');
+    it(`Clicks the appropriate button`, function () {
+        cy.get('[tripnav~="wrapper"]').find('#trip-nav-ul-li-2').click();
+    });
+    checkVisibility('[ctest=utm_itinerary]', '#form-itinerary');
+    submitForm('#form-itinerary-submit');
+    checkValidity('#form-itinerary');
+    requiredInfo('#form-itinerary');
+    checkbox('#form-itinerary', '#requestCatalog');
+    shipInfo('#form-itinerary');
+    submitForm('#form-itinerary-submit');
+    utmPersonalInfoTrue();
+    utmSecondaryTrue();
+    catTemp();
+    utmOptInTrue();
+    utmValueTrue();
 
-// });
+});
