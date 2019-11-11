95d29dd519ad91413d751b0bb485e5020e34aef6
Alex Thornton <alext@nathab.com>
2019-11-11 15:35:20 -0700

    testing on alex

--- cypress/integration/ask_cta_spec.js
+++ cypress/integration/ask_cta_spec.js
@@ -42,7 +42,7 @@ describe(`Ask Form Test - eNews checked`, function () {
 
 describe(`Ask Form Test - nothing checked`, function () {
     //Validity Check
-    getURL('https://dev.', '');
+    getURL('alex.', '');
     clickCTA(`${cta_buttons.id}`);
     checkVisibility('[ctest=ask_cta_lightbox]', '#form-ask');
     submitForm('#form-ask-submit');
